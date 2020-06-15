ef0699999cde0ef8347e2f4bdbf942112b8b6d93
Rylan Cole <rylancole@Rylans-Air.hitronhub.home>
2020-06-15 09:18:29 -0700

    Creating page object

--- OcwaWrap.py
+++ OcwaWrap.py
@@ -52,6 +52,18 @@ class OcwaWrap:
         response = self.sendQuery({ 'query' : ' { pages { single(id: '+str(id)+') { '+attributes+'} } }' })
         return response
 
+    def createPage(self, id, called_attrs=SINGLE_ATTRS):
+        response = self.sendSingleQuery(id, called_attrs)
+        if('data' in response.keys()):
+            if('pages' in response['data'].keys() and 'single' in response['data']['pages'].keys()):
+                return Page(response['data']['pages']['single'])
+            else:
+                raise Exception("Issue accessing page data.")
+        elif('errors' in response.keys() and len(response['errors']) > 0 and 'message' in response['errors'][0].keys()):
+            raise Exception(response['errors'][0]['message']) 
+        else:
+            raise Exception("Unkown error")
+
     def slack(self, msg):
         if(self.slack_url):
             command = os.popen('''curl -X POST -H 'Content-type: application/json' --data '{"text":"'''+msg+'''"}' '''+self.slack_url)
@@ -59,3 +71,77 @@ class OcwaWrap:
             print(command.close())
         else:
             raise Exception("No access to slack messaging, must have SLACK_URL")
+
+class Page:
+
+    def __init__(self, pageObject):
+        self.attr = pageObject
+
+    def get(self, key):
+        if(key in self.attr.keys()):
+            return self.attr[key]
+        else:
+            raise Exception("You did not request access to "+key)
+
+    def getAuthorEmail(self):
+        return self.get("authorEmail")
+
+    def getAuthorId(self):
+        return self.get("authorId")
+
+    def getAuthorName(self):
+        return self.get("authorName")
+
+    def getContent(self):
+        return self.get("content")
+
+    def getContentType(self):
+        return self.get("contentType")
+
+    def getCreatedAt(self):
+        return self.get("createdAt")
+
+    def getCreatorEmail(self):
+        return self.get("creatorEmail")
+
+    def getCreatorId(self):
+        return self.get("creatorId")
+
+    def getCreatorName(self):
+        return self.get("creatorName")
+
+    def getDescription(self):
+        return self.get("description")
+
+    def getEditor(self):
+        return self.get("editor")
+        
+    def getId(self):
+        return self.get("id")
+
+    def getIsPublished(self):
+        return self.get("isPublished")
+
+    def getIsPrivate(self):
+        return self.get("isPrivate")
+
+    def getPath(self):
+        return self.get("path")
+
+    def getPrivateNS(self):
+        return self.get("privateNS")
+
+    def getPublishEndDate(self):
+        return self.get("publishEndDate")
+
+    def getPublishStartDate(self):
+        return self.get("publishStartDate")
+
+    def getRender(self):
+        return self.get("render")
+
+    def getTitle(self):
+        return self.get("title")
+
+    def getUpdatedAt(self):
+        return self.get("updatedAt")
---
+++ test-createPage.py
@@ -0,0 +1,51 @@
+from OcwaWrap import OcwaWrap
+import json
+import os
+import sys
+
+def main():
+
+    if(len(sys.argv) > 1 and sys.argv[1] in os.environ.keys()):
+        API_TOKEN = os.environ[sys.argv[1]]
+    elif('OCWA_TOKEN' in os.environ.keys()):
+        API_TOKEN = os.environ['OCWA_TOKEN']
+    else:
+        print("---")
+        print("Token missing from enviroment variables.")
+        print("Please ensure the variable is named:")
+        print("OCWA_TOKEN or enter another name as an arguement:")
+        print("```python3 test-token.py VAR_NAME```")
+        print("---")
+        return
+
+    wrapper = OcwaWrap(API_TOKEN)
+    page = wrapper.createPage(2)
+
+    page.getAuthorEmail()
+    page.getAuthorId()
+    page.getAuthorName()
+    page.getContent()
+    page.getContentType()
+    page.getCreatedAt()
+    page.getCreatorEmail()
+    page.getCreatorId()
+    page.getCreatorName()
+    page.getDescription()
+    page.getEditor()
+    page.getId()
+    page.getIsPublished()
+    page.getIsPrivate()
+    page.getPath()
+    page.getPrivateNS()
+    page.getPublishEndDate()
+    page.getPublishStartDate()
+    page.getRender()
+    page.getTitle()
+    page.getUpdatedAt()
+
+    print("---")
+    print("Connection success.")
+    print("---")
+
+if __name__ == "__main__":
+    main()
